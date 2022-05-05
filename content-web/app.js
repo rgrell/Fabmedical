02989ac6717ae3c4badf88e7a19c75383236fc94
rgrell <rg@grell.at>
2022-05-05 13:12:05 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("067f7f9d-f8f5-42bc-8145-94957f7b24c4");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
